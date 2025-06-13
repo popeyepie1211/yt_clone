bbadee29fd464b1ffb265b03c5cfe36a9132348a
ARYA SHEWALE <aryashewale18@gmail.com>
2025-06-13 18:39:11 +0530

    CONNECT TO DB ATLAS

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constant.js"
+// import express from "express"
+
+import dotenv from "dotenv"
+import connectDB from "../db/index.js";
+// import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+// const app= express()
+
+// (async ()=>{
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("ERROR",(error)=>{
+//             console.log("ERRR: ",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
+//         })
+//     }
+//     catch(error){
+//         console.error("ERROR: ",error)
+//         throw error
+//     }
+// })()
